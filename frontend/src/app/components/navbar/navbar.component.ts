1c1aa5ac1b7bbd4c7481a8f14ab31a6f84c75d95
IsaacAndPartner <isaacandpartner@gmail.com>
2023-06-26 04:56:25 -0500

    created routes and profile component

--- frontend/src/app/components/navbar/navbar.component.ts
+++ frontend/src/app/components/navbar/navbar.component.ts
@@ -1,28 +1,61 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  OnDestroy,
+  AfterViewChecked
+} from '@angular/core';
 import { Offcanvas } from 'bootstrap';
 import { AuthService } from 'src/app/services/auth.service';
+import { Subscription } from 'rxjs';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })
-export class NavbarComponent implements OnInit {
+export class NavbarComponent implements OnInit, OnDestroy, AfterViewChecked {
   isAuthenticated!: boolean;
-  constructor(private authService: AuthService) { }
+  private authSubscription!: Subscription;
+  private isLinksInitialized = false;
+
+  constructor(private authService: AuthService, private router: Router) {}
+
+  ngOnInit(): void {
+    this.authSubscription = this.authService
+      .getAuthStatus()
+      .subscribe((isAuthenticated) => {
+        this.isAuthenticated = isAuthenticated;
+      });
+  }
+
+  ngAfterViewChecked(): void {
+    if (!this.isLinksInitialized) {
+      this.initializeLinkItems();
+    }
+  }
 
   initializeLinkItems(): void {
     const linkItems: NodeListOf<Element> = document.querySelectorAll(".link-item");
     const indicator: HTMLElement = document.querySelector(".indicator") as HTMLElement;
 
-    linkItems.forEach((linkItem: Element, index: number) => {
-      linkItem.addEventListener("click", () => {
-        document.querySelector(".active")!.classList.remove("active");
-        linkItem.classList.add("active");
+    if (linkItems.length > 0) {
+      this.isLinksInitialized = true;
+      linkItems.forEach((linkItem: Element, index: number) => {
+        linkItem.addEventListener("click", () => {
+          document.querySelector(".active")!.classList.remove("active");
+          linkItem.classList.add("active");
 
-        indicator.style.left = `${index * 95 + 48}px`;
+          indicator.style.left = `${index * 95 + 48}px`;
+        });
       });
-    });
+    }
+  }
+
+  ngOnDestroy() {
+    if (this.authSubscription) {
+      this.authSubscription.unsubscribe();
+    }
   }
 
   openOffCanvas(offcanvasId: string) {
@@ -31,14 +64,9 @@ export class NavbarComponent implements OnInit {
     offCanvas.show();
   }
 
-  ngOnInit(): void {
-    this.initializeLinkItems();
-    this.isAuthenticated = this.authService.isAuthenticated();
-  }
-
-
   logout() {
     this.authService.logout();
     this.isAuthenticated = false;
+    this.router.navigate(['/eventowasimodo']);
   }
 }
--- frontend/src/app/services/auth.service.ts
+++ frontend/src/app/services/auth.service.ts
@@ -1,15 +1,21 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { tap } from 'rxjs/operators';
-import { Observable } from 'rxjs';
+import { Observable, BehaviorSubject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
   private url_api = 'http://localhost:4002';
+  private authStatus = new BehaviorSubject<boolean>(this.isAuthenticated());
+
 
   constructor(private http: HttpClient) {}
+
+  getAuthStatus(): Observable<boolean> {
+    return this.authStatus.asObservable();
+  }
 
   login(userName: string, password: string): Observable<{ token: string }> {
     return this.http
@@ -17,13 +23,14 @@ export class AuthService {
       .pipe(
         tap(({ token }) => {
           localStorage.setItem('auth_token', token);
+          this.authStatus.next(true);
         })
       );
   }
-
-  // Método para cerrar sesión
+  
   logout() {
     localStorage.removeItem('auth_token');
+    this.authStatus.next(false);
   }
 
   // Método para verificar si el usuario está autenticado
